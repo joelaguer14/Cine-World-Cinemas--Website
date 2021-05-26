f0c6c3e9eebd75085ec0d7e447bc6c41dae3db23
Felipe Guzmán <feliguz0209@gmail.com>
2021-05-26 10:17:06 -0600

    feat: HibernateUtil class and gradle build configuration added

--- CineWorldCinemas/src/main/java/CineWorldCinemas/data/HibernateUtil.java
+++ CineWorldCinemas/src/main/java/CineWorldCinemas/data/HibernateUtil.java
@@ -5,10 +5,73 @@
  */
 package CineWorldCinemas.data;
 
+import CineWorldCinemas.logic.*;
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+        
 /**
  *
  * @author joela
  */
 public class HibernateUtil {
-    
+
+    private static SessionFactory sessionFactory;
+
+    /**
+     * Class utils for connection to the database using Hibernate
+     *
+     * @return sessionFactory
+     */
+    private HibernateUtil() {
+    }
+
+    public static synchronized SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/demo?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "none");
+
+                // IMPORTANT HBM2DDL_AUTO +++++++++++++
+                // validate: validate the schema, makes no changes to the database.
+                // update: update the schema.
+                // create: creates the schema, destroying previous data.
+                // create-drop: drop the schema when the SessionFactory is closed explicitly, typically when the application is stopped.
+                // none: does nothing with the schema, makes no changes to the database
+                configuration.addAnnotatedClass(Administrator.class);
+                configuration.addAnnotatedClass(Auditorium.class);
+                configuration.addAnnotatedClass(Client.class);
+                configuration.addAnnotatedClass(Movie.class);
+                configuration.addAnnotatedClass(Screening.class);
+                configuration.addAnnotatedClass(Seat.class);
+                configuration.addAnnotatedClass(SeatReserved.class);
+                configuration.addAnnotatedClass(Ticket.class);
+                configuration.addAnnotatedClass(User.class);
+                configuration.setProperties(settings);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                //sessionFactory.openSession();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 }
