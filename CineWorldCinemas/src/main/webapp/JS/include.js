ce4672cd8ad7b123a84459a0c21ed13c69331209
Felipe Guzmán <feliguz0209@gmail.com>
2021-05-28 11:19:15 -0600

    feat: include.js added and include function created

---
+++ CineWorldCinemas/src/main/webapp/JS/include.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function () {
+    var includes = $('[data-include]');
+    jQuery.each(includes, function () {
+        var file = 'presentation/' + $(this).data('include') + '.html';
+        $(this).load(file);  
+    });
+});
+
