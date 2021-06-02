ad2def28a6a90803cf61fa27900f4db56084e8bf
Felipe Guzmán <feliguz0209@gmail.com>
2021-06-01 22:40:24 -0600

    fix: login pop up updated

--- CineWorldCinemas/src/main/webapp/JS/login.js
+++ CineWorldCinemas/src/main/webapp/JS/login.js
@@ -3,3 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+$('.li-modal').on('click', function (e) {
+    e.preventDefault();
+    $('#login-modal').modal('show').find('.modal-content').load($(this).attr('href'));
+});
