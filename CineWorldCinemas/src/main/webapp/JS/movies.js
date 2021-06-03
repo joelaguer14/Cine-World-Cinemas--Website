78a8d4b5cad6b2d3c6f5925ac330ef46b142b059
Manfred Joel Agüero Campos <46728129+joelaguer14@users.noreply.github.com>
2021-06-02 23:44:49 -0600

    feat: show screenings

--- CineWorldCinemas/src/main/webapp/JS/movies.js
+++ CineWorldCinemas/src/main/webapp/JS/movies.js
@@ -8,17 +8,25 @@ var movies = [
     {
         title: "Spiderman",
         imagesrc: "https://pauladeveraescritora.files.wordpress.com/2019/10/spider-man-far-from-home-banner.jpg",
-        description: "Parker is recruited by Nick Fury and Mysterio to face the Elementals while he is on a school trip to Europe."
+        description: "Parker is recruited by Nick Fury and Mysterio to face the Elementals while he is on a school trip to Europe.",
+        screenings: ["Jun 28, 3pm/ A2", "Jun 28, 6pm/ B2", "Jun 29, 3pm/ A2","Jun 28, 3pm/ A2", "Jun 28, 6pm/ B2", "Jun 29, 3pm/ A2","Jun 28, 3pm/ A2", "Jun 28, 6pm/ B2", "Jun 29, 3pm/ A2"]
     },
     {
         title: "Batman",
         imagesrc: "http://splashreport.com/wp-content/uploads/2017/01/legobatman-header.png",
-        description: "In the Lego Universe, Batman continues to protect Gotham City and fight crime. During his latest mission to stop Joker from destroying the city, he hurts his arch-rival's feelings by telling him he is not as important in his life as he thinks he is, leading Joker to seek the ultimate revenge on him.In the Lego Universe, Batman continues to protect Gotham City and fight crime. During his latest mission to stop Joker from destroying the city, he hurts his arch-rival's feelings by telling him he is not as important in his life as he thinks he is, leading Joker to seek the ultimate revenge on him"
+        description: "In the Lego Universe, Batman continues to protect Gotham City and fight crime. During his latest mission to stop Joker" +
+                " from destroying the city, he hurts his arch-rival's feelings by telling him he is not as important in his life as he thinks he " +
+                "is, leading Joker to seek the ultimate revenge on him.In the Lego Universe, Batman continues to protect Gotham City and fight crime." +
+                " During his latest mission to stop Joker from destroying the city, he hurts his arch-rival's feelings by telling him he is not as important" +
+                " in his life as he thinks he is, leading Joker to seek the ultimate revenge on him",
+        screenings: ["Jun 28, 5pm/ B3", "Jun 29, 6pm/ A3", "Jun 30, 2pm/ C2"]
     },
     {
         title: "Superman",
         imagesrc: "https://collider.com/wp-content/uploads/man-of-steel-poster-banner.jpg",
-        description: "Set in 1981, it follows Arthur Fleck, a failed clown and stand-up comedian whose descent into insanity and nihilism inspires a violent counter-cultural revolution against the wealthy in a decaying Gotham City."
+        description: "Set in 1981, it follows Arthur Fleck, a failed clown and stand-up comedian whose descent into insanity and nihilism inspires a violent counter" +
+                "-cultural revolution against the wealthy in a decaying Gotham City.",
+        screenings: ["Jun 29, 4pm/ C1", "Jun 30, 5pm/ A1", "Jul 1, 2pm/ C1"]
     }
 ];
 
@@ -64,7 +72,8 @@ function rowContentDisplay(rowContent, movie) {
     rowContent.append(
             "<div class='col'>" +
             "<div class='card shadow-sm my-card'>" +
-            "<img class='image-grid d-block w-100' src='" + movie.imagesrc + "' alt=''>" +
+            "<img class='image-grid d-block w-100 movie-image' src='" + movie.imagesrc + "' alt=''>" +
+            "<div class='screenings'> <p>" + movie.screenings + "</p></div>" +
             "<div class='card-body'>" +
             "<div class='d-flex justify-content-between align-items-center'>" +
             "<div class='btn-group'>" +
