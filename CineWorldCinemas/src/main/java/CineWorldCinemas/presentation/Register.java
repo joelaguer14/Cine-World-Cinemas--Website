70d5ddeabd526761413ac390a94d77469732b1b5
alonso-dev <alonso10es@gmail.com>
2021-06-08 14:59:38 -0600

    feat: register auditorium implemented

--- CineWorldCinemas/src/main/java/CineWorldCinemas/presentation/Register.java
+++ CineWorldCinemas/src/main/java/CineWorldCinemas/presentation/Register.java
@@ -5,10 +5,12 @@
  */
 package CineWorldCinemas.presentation;
 
+import CineWorldCinemas.logic.Auditorium;
+import CineWorldCinemas.logic.Movie;
+import CineWorldCinemas.logic.Screening;
 import CineWorldCinemas.logic.Service;
 import CineWorldCinemas.logic.User;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.NotAcceptableException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
@@ -19,11 +21,35 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/register")
 public class Register {
-    
+
     @POST
-    @Consumes(MediaType.APPLICATION_JSON) 
-    public void register(User user) throws Exception {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void registerUser(User user) throws Exception {
         Service.instance().saveUser(user);
-     
+
+    }
+
+    @POST
+    @Path("auditorium")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void registerAuditorium(Auditorium auditorium) throws Exception {
+        Service.instance().saveAuditorium(auditorium);
+
+    }
+
+    @POST
+    @Path("movie")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void registerMovie(Movie movie) throws Exception {
+        Service.instance().saveMovie(movie);
+
+    }
+
+    @POST
+    @Path("screening")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void registerScreening(Screening screening) throws Exception {
+        Service.instance().saveScreening(screening);
+
     }
 }
--- CineWorldCinemas/src/main/webapp/JS/registerAuditoriums.js
+++ CineWorldCinemas/src/main/webapp/JS/registerAuditoriums.js
@@ -4,7 +4,52 @@
  * and open the template in the editor.
  */
 
-function(){
-    return null;
-};
+var auditorium = {name: "", seatsNumber: 0};
+var url = "http://localhost:8080/CineWorldCinemas/";
 
+function reset() {
+   auditorium = {name: "", seatsNumber: 0};
+}
+
+function validate() {
+    var error = false;
+    $("#register-auditorium-form input").removeClass("invalid");
+    error |= $("#register-auditorium-form input[type='text']").filter((i, e) => {
+        return e.value === '';
+    }).length > 0;
+    $("#register-auditorium-form input[type='text']").filter((i, e) => {
+        return e.value === '';
+    }).addClass("invalid");
+    return !error;
+}
+function load() {
+    auditorium = Object.fromEntries((new FormData($("#register-auditorium-form").get(0))).entries());
+}
+function add() {
+    load();
+    console.log(auditorium);
+    if (!validate()) {
+        console.log("validate");
+        return;
+    }
+    let request = new Request(url + 'api/register/auditorium', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(auditorium)});
+    (async () => {
+        const response = await fetch(request);
+        if (!response.ok) {
+            errorMessage(response.status, $("#register-auditorium-form #error"));
+            return;
+        }
+        reset();
+        render();
+    })();
+}
+function render(){
+    $("#register-auditorium-name").val(auditorium.name);
+    $("#register-auditorium-seats-quantity").val(""); 
+}
+
+function loaded() {
+    $("#register-auditorium-button").click(add);
+}
+
+$(loaded);  
